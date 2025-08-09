33eb288c8e453b35011241e3432af52231ef9ec3
ozefear <ozanakpinar@hacettepe.edu.tr>
2025-08-10 00:41:22 +0300

    improvements

--- src/app/api/events/[eventId]/photos/[photoId]/route.js
+++ src/app/api/events/[eventId]/photos/[photoId]/route.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import { getFirebaseClient } from "@/lib/firebaseClient";
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, getDoc, deleteDoc } from "firebase/firestore";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
